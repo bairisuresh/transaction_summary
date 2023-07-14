71a26170b506b977c2ace5e4d2e4d2d9e057cd83
Bairi <suresh.bairi@metlife.com>
2023-07-13 22:19:39 -0400

    changes

---
+++ src/main/java/com/customer/details/transactionSummary/TransactionSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.customer.details.transactionSummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSummaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/details/transactionSummary/TransactionSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.details.transactionSummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSummaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
